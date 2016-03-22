e33e316e7384487f792d9085cdc446d910079735
mosamar <mosamar@mosamars-MacBook-Air.local>
2016-03-22 18:52:21 -0500

    This should meet all the requirements. Just needs clean up and additional documentation.

--- driver.cpp
+++ driver.cpp
@@ -13,22 +13,23 @@ using namespace std;
 int main()
 {
     
-    string postFixExp;
+    string postFixExp;                                      // user expresssion entry
     char token, anotherExp;
-    int var1, var2, i;
-    bool badVal;
+    int var1, var2, i, lastItem;
+    bool malform;
 
 
 
     do
     {
-        Stack RpnStack;
-        badVal = false;                                     // boolean for unexpected characters
+        Stack RpnStack;                                     // object decleration inside loop to 'reconstruct' object each time
+        malform = false;                                     // boolean for unexpected characters
         cout << "Enter post fix expression: ";
         getline(cin, postFixExp);                           // get user expression
         i = 0;
         
-        while(badVal == false && i < postFixExp.length())  // loop over length of user expression
+        // this loop itereates over the lenght of the user entered expression and while a malform flag isn't rasised
+        while(malform == false && i < postFixExp.length())
         {
             token = postFixExp[i];
 
@@ -37,19 +38,19 @@ int main()
             case ' ':
                 break;
 
-                case '0':case '1':case '2':case '3':case '4':
+                case '0':case '1':case '2':case '3':case '4':   // this will handle the operaand digit cases
                 case '5':case '6':case '7':case '8':case '9':
                     RpnStack.push(token - 48);
                     break;
 
-                case '+':case '-':case '*':case '/':
+                case '+':case '-':case '*':case '/':            // this will hand the opeartor cases
                     var1 = RpnStack.top();
                     RpnStack.pop();
                     
-                    if (RpnStack.empty())
+                    if (RpnStack.empty())                       // check for stack less than two items
                     {
                         cout << "Error --> Stack does not contain two items -- Malformed expression." << endl;
-                        badVal = true;
+                        malform = true;
                         break;
                     }
                     
@@ -66,27 +67,35 @@ int main()
                     else if (token == '*')
                         RpnStack.push(var2 * var1);
                     else if (token == '/')
-                        if (var1 == 0)
+                        if (var1 == 0)                          // check for zero denominator
                         {
                             cout << "Error --> Cannot divide by zero." << endl;
-                            badVal = true;
+                            malform = true;
                             break;
                         }
                     else
+                    {
                         RpnStack.push(var2 / var1);
+                    }
                     break;
                     
-                default:
+                default: //default case
                     cout << "Error: --> Malformed Expression encountered" << endl;
-                    badVal = true;
+                    malform = true;
                     break;
             }
             i++;
-        }
-        if (badVal == false)
-            cout << endl << "Value for expression entered is: " << RpnStack.top() << endl;
 
-        cout << endl << "More Expressions (Y - Yes : any other key to quit)? ";
+        }
+        lastItem = RpnStack.top();
+        RpnStack.pop();
+        
+        if (malform == false && !RpnStack.empty())
+        {
+            cout << endl << "Value for expression entered is: " << lastItem << endl;
+        }
+
+        cout << endl << "More Expressions (y - yes : any other key to quit)? ";
         cin.get(anotherExp);
         cin.ignore();
         cout << endl << "--------------------------------------" << endl;
