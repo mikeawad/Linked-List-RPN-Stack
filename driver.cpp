a597a97f2e123804bcbde45bb9d760cff6b39a05
mikeawad <awadbigmo@gmail.com>
2016-03-22 08:07:30 -0500

    Updated driver file
    Updated:
    - added blank token switch case
    - added default

--- driver.cpp
+++ driver.cpp
@@ -14,40 +14,63 @@ int main()
 {
     Stack RpnStack;
     string postFixExp;
-    char token;
+    char token, anotherExp;
     int var1, var2;
+    bool badVal;
 
-    cout << "Enter post fix expression: ";
-    cin >> postFixExp;
 
-    for (int i = 0; i < postFixExp.length(); i++)
+
+    do
     {
-        token = postFixExp[i];
-
-        switch (token)
+        badVal = false;                                  // boolean for unexpected characters
+        cout << "Enter post fix expression: ";
+        getline(cin, postFixExp);                        // get user expression
+        for (int i = 0; i < postFixExp.length(); i++)    // loop over length of user expression
         {
-        case '0':case '1':case '2':case '3':case '4':case '5':case '6':case '7':case '8':case '9':
-            RpnStack.push(token - 48);
-            break;
-        case '+':case '-':case '*':case '/':
-            var1 = RpnStack.top();
-            RpnStack.pop();
-            var2 = RpnStack.top();
-            RpnStack.pop();
+            token = postFixExp[i];
 
-            if (token == '+')
-                RpnStack.push(var2 + var1);
-            else if (token == '-')
-                RpnStack.push(var2 - var1);
-            else if (token == '*')
-                RpnStack.push(var2 * var1);
-            else if (token == '/')
-                RpnStack.push(var2 / var1);
-            break;
+            switch (token)
+            {
+            case ' ':
+                break;
+
+                case '0':case '1':case '2':case '3':case '4':
+                case '5':case '6':case '7':case '8':case '9':
+                    RpnStack.push(token - 48);
+                    break;
+
+                case '+':case '-':case '*':case '/':
+                    var1 = RpnStack.top();
+                    RpnStack.pop();
+
+                    var2 = RpnStack.top();
+                    RpnStack.pop();
+
+                    if (token == '+')
+                        RpnStack.push(var2 + var1);
+                    else if (token == '-')
+                        RpnStack.push(var2 - var1);
+                    else if (token == '*')
+                        RpnStack.push(var2 * var1);
+                    else if (token == '/')
+                        RpnStack.push(var2 / var1);
+                    break;
+                default:
+                    cout << "Malformed Expression encountered";
+                    badVal = true;
+                    break;
+            }
 
         }
+        if (badVal == false)
+            cout << endl << "Value for expression entered is: " << RpnStack.top() << endl;
 
-    }
-    cout << endl << "Value for expression entered is: " << RpnStack.top() << endl;
+        cout << endl << "More Expressions (Y - Yes : any other key to quit)? ";
+        cin.get(anotherExp);
+        cin.ignore();
+        cout << endl << "--------------------------------------" << endl;
+    }while (anotherExp == 'y' || anotherExp == 'Y');
     return 0;
 }
+
+
